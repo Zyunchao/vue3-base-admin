62937f7ecbac2058c434550bf468e04f94b1b1ee
Zyunchao <18651805393@163.com>
2024-01-26 15:02:44 +0800

    feat(config): 运行时 eslint 配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
+import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
+        eslintPlugin(),
         AutoImport({
             resolvers: [ElementPlusResolver()],
             imports: ['vue', 'vue-router']
